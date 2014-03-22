8fa60b9b513709352733c9c2af119d5149f7a20b
bushiko <bushiiko@gmail.com>
2014-03-21 18:39:33 -0600

    -Estructura de proyecto. -Primer bosquejo y colores (¿?)

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<style type="text/css">
+
+
+		body {
+			padding: 0;
+			margin: 0;
+			background: #f2f2f2;
+/*			background: #8b190c;
+			background: #e4542d;
+			background: -moz-radial-gradient(center,ellipse cover,#e4542d 0%,#8b190c 100%);
+			background: -webkit-gradient(radial,center center,0px,center center,100%,color-stop(0%,#e4542d),color-stop(100%,#8b190c));
+			background: -webkit-radial-gradient(center,circle cover,#e4542d,#8b190c);
+			background: -o-radial-gradient(center,circle cover,#e4542d,#8b190c);
+			background: -ms-radial-gradient(center,circle cover,#e4542d,#8b190c);
+			background: radial-gradient(circle at center,#e4542d,#8b190c);*/
+			font-family: Candara, Calibri, Segoe, "Segoe UI", Optima, Arial, sans-serif;
+		}
+
+		header .center {
+			max-width: 960px;
+			position: relative;
+			margin: 0 auto;
+		}
+
+		header .logo {
+			border-radius: 50%;
+			width: 200px;
+			height: 200px;
+			background: #FFF;
+			display: inline-block;
+			font-size: 4em;
+			line-height: 3em;
+			text-align: center;
+		}
+
+		header .slogan {
+			/*color: #fff;*/
+			color: #606060;
+			font-weight: 100;
+			font-size: 42px;
+			text-align: center;
+			margin: 0 100px;
+		}
+
+		.content {
+			margin-top: 100px;
+			height: 300px;
+			display: block;
+			font-size: 2em;
+			background: #00c2dc;
+			/*background: rgba(255, 255, 255, 0.28);*/
+			text-align: center;
+			color: #FFF;
+		}
+		</style>
+	</head>
+	<body>
+
+		<header>
+			<div class="center">
+			<div class="logo">Lens_</div>
+			<div class="slogan">Building WebApps since 1991</div>
+			</div>
+		</header>
+
+		<div class="content">
+			<h1>Some content!</h1>
+		</div>
+
+	</body>
+</html>
