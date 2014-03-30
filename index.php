b6f245e0b1184ab93c38a2a68f88dd1b5f4ce4c3
bushiko <bushiiko@gmail.com>
2014-03-29 20:06:37 -0600

    -
    Signed-off-by: bushiko <bushiiko@gmail.com>

--- index.php
+++ index.php
@@ -1,106 +1,90 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,400italic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,400italic' rel='stylesheet' type='text/css'>
         <link type="text/css" href="libs/bootstrap/css/bootstrap.css" rel="stylesheet">
-<<<<<<< .mine
-        <script type="text/javascript" src="js/jQuery.js"></script>
+
+        <script type="text/javascript" src="js/scrolly.js"></script>
         <script type="text/javascript" src="js/scrolly.js"></script>
         <script>
             $(function (){
                 $('.parallax').scrolly({bgParallax: true});
             });
         </script>
-=======
-            <script type="text/javascript" src="js/scrolly.js"></script>
-            <script type="text/javascript" src="js/scrolly.js"></script>
-            <script>
-                $(function (){
-                    $('.parallax').scrolly({bgParallax: true});
-                });
-            </script>
->>>>>>> .theirs
+
 		<style type="text/css">
+			body {
+				padding: 0;    
+				margin: 0;
+				background: #f2f2f2;
+				font-family: 'Roboto', Calibri, Segoe, "Segoe UI", Optima, Arial, sans-serif;
+				background: url("img/bridge.jpg");
+				background-size: cover;
+	            color: #555;
+			}
+
+	        #item{
+	            background: url('img/test.jpeg');
+	        }
+
+			header .center {
+				max-width: 960px;
+				position: relative;
+				margin: 0 auto;
+			}
+
+			header .logo {
+				border-radius: 50%;
+				width: 200px;
+				height: 200px;
+				background: #FFF;
+				display: inline-block;
+				font-size: 4em;
+				line-height: 3em;
+				text-align: center;
+			}
+
+			header .slogan {
+				color: #FFF;
+				/*color: #606060;*/
+				font-weight: 300;
+				font-size: 4em;
+				text-align: center;
+				margin: 0 100px;
+			}
+
+			.content {
+				margin-top: 100px;
+				height: 200px;
+				display: block;
+				font-size: 2em;
+				background: #FFF;
+				/*background: #00c2dc;*/
+				/*background: rgba(255, 255, 255, 0.28);*/
+				text-align: center;
+				/*color: #FFF;*/
+			}
 
 
-		body {
-			padding: 0;    
-			margin: 0;
-			background: #f2f2f2;
-			font-family: 'Roboto', Calibri, Segoe, "Segoe UI", Optima, Arial, sans-serif;
-			background: url("img/bridge.jpg");
-            color: #555;
-		}
+			.content h2 {
+				font-weight: 300;
+				font-size: 80px;
 
-        #item{
-            background: url('img/test.jpeg');
-        }
-
-		header .center {
-			max-width: 960px;
-			position: relative;
-			margin: 0 auto;
-		}
-
-		header .logo {
-			border-radius: 50%;
-			width: 200px;
-			height: 200px;
-			background: #FFF;
-			display: inline-block;
-			font-size: 4em;
-			line-height: 3em;
-			text-align: center;
-		}
-
-		header .slogan {
-			color: #FFF;
-			/*color: #606060;*/
-			font-weight: 300;
-			font-size: 4em;
-			text-align: center;
-			margin: 0 100px;
-		}
-
-		.content {
-			margin-top: 100px;
-			height: 200px;
-			display: block;
-			font-size: 2em;
-			background: #FFF;
-			/*background: #00c2dc;*/
-			/*background: rgba(255, 255, 255, 0.28);*/
-			text-align: center;
-			/*color: #FFF;*/
-		}
-
-
-		.content h2 {
-			font-weight: 300;
-			font-size: 80px;
-
-		}
+			}
 		</style>
 	</head>
         <body>
-            <section id="bottle">
-                <article>
-                    <h1>Our products are a load of rubbish.</h1>   
-                    <p><strong>NIKE FREE TR FIT</strong>
-                        Each Nike 2010 World Championship Football jersey is made entirely from recycled polyester. That's up to eight recycled plastic bottles per jersey. In total, that diverted nearly 13 million plastic bottles from landfills. Yes, you read right. 13 million. Plastic. Bottles.</p>    
-                </article>
-                <div class="parallax" data-velocity="-.3"></div>
-            </section>
-		<header>
-			<div class="center">
-			<div class="logo">Lens_</div>
-			<div class="slogan">Building WebApps since 1991</div>
-			</div>
-		</header>
 
-		<div class="content">
-			<div>Some content!</div>
-		</div>
+			<header>
+				<div class="center">
+					<div class="logo">Lens_</div>
+					<div class="slogan">Building WebApps since 1991</div>
+				</div>
+			</header>
+
+			<div class="content">
+				<div>Some content!</div>
+			</div>
             <div id="item" class="parallax" data-velocity="-.3"></div>
 	</body>
 </html>
