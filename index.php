276f4236c52bd374f43b4e8f495eedf53e4fc7a3
bushiko <bushiiko@gmail.com>
2014-04-03 10:08:40 -0600

    -Navbar -Removido onescroll-page. Tampoco servía en Android :c

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML>
+﻿<!DOCTYPE HTML>
 <html>
 	<head>
 		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700' rel='stylesheet' type='text/css'>
@@ -6,27 +6,26 @@
 
         <script type="text/javascript" src="js/jquery.1.11.0.min.js"></script> 
         <script type="text/javascript" src="libs/bootstrap/js/bootstrap.js"></script>
-        <script type="text/javascript" src="libs/onepage-scroll/jquery.onepage-scroll.js"></script> 
-        <link type="text/css" href="libs/onepage-scroll/onepage-scroll.css" rel="stylesheet">
 
 		<script>
 		  $(document).ready(function(){
-				$(".main-content").onepage_scroll({
-				   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-				   easing: "ease-out",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", 
-				                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-				   animationTime: 500,             // AnimationTime let you define how long each section takes to animate
-				   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-				   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-				   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-				   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-				   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-				   keyboard: true,                  // You can activate the keyboard controls
-				   responsiveFallback: 600        // You can fallback to normal page scroll by defining the width of the browser in which
-				                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever 
-				                                    // the browser's width is less than 600, the fallback will kick in.
-				});
-			});
+
+		  	window.addEventListener("resize", function webViewerResize(evt) {
+		  		resizeSections();
+		  	});
+
+		  	resizeSections();
+		});
+			
+		function resizeSections() {
+			var MIN_SECTION_HEIGHT = 700;
+	  		dHeight = document.documentElement.clientHeight;
+	  		[].forEach.call(document.querySelectorAll('section'), function(e) {
+	  			if(!e.classList.contains('no-resize'))
+	  				e.style.minHeight = (dHeight < 700 ? MIN_SECTION_HEIGHT : dHeight)+'px';
+	  		});
+		}
+
 		</script>
 
 		<style type="text/css">
@@ -39,14 +38,23 @@
 			}
 
 			section {
-				height: 700px;
 				overflow: hidden;
 			}
 
-			#slide-0{
+			#slide-0 {
 				background-color: #FFF;
 				background-image: url('img/lens_2.jpg');
 				background-size: cover;
+				height: 1px; /*hack para hacer que los row child obtengan height en porcentaje*/
+			}
+
+			#slide-0 .row {
+				height: 20%;
+			}
+
+			#slide-0 .row:first-child {
+				padding-top: 100px;
+				height: 60%;
 			}
 
 			#slide-0 .logo {
@@ -61,9 +69,6 @@
 				font-weight: 200;
 				line-height: 3.3em;
 				text-align: center;
-				margin: 50px 0 200px 0;
-
-				/*border: 5px solid #555;*/
 			}
 
 			#slide-0 .slogan {
@@ -71,9 +76,6 @@
 				font-size: 4em;
 				color: #FFF;
 				margin-bottom: 20px;
-				/*text-shadow: 2px 2px 5px rgba(255, 255, 255, 1);*/
-				/*text-align: center;*/
-				/*margin: 0 100px;*/
 			}
 
 			@media all and (max-width: 580px) {
@@ -81,8 +83,8 @@
 					font-size: 2em;
 				}
 
-				#slide-0 .logo {
-					margin-bottom: 100px;
+				#slide-0 .row:first-child {
+					height: 40%;
 				}
 
 				#slide-0 .lens-btn {
@@ -92,20 +94,27 @@
 			}
 
 
-			#slide-1 {
-				background: none;
-				/*background-image: url('img/bridge2.jpg');*/
+			#slide-1{
+				background: rgb(31,32,54);
 			}
 
 
-			#slide-2{
-				background: rgb(31,32,54);
+			#slide-2 {
+				background: none;
+				padding-top: 100px;
+			}
+			#slide-2 img {
+				max-height: 100px;
+			}
+
+			#slide-3 {
+				background: rgb(147,210,220);
 			}
 
 			#slide-5 {
 				background: #EEE;
 				border-top: 1px solid #CCC;
-/*				height: 200px !important;*/
+				height: 200px !important;
 			}
 
 			nav {
@@ -117,7 +126,7 @@
 				font-size: 4em;
 			}
 
-/*			taken from http://www.polymer-project.org/*/		
+			/*taken from http://www.polymer-project.org/*/		
 			.lens-btn {
 				display: inline-block;
 				-webkit-transition: box-shadow 0.2s, background 0.2s;
@@ -143,58 +152,96 @@
 				box-shadow: 0px 4px 10px 0 rgba(0,0,0,0.1),0px 2px 10px 0px rgba(0,0,0,0.1);
 			}
 
+			.lens-nav {
+				padding: 5px 0;
+				background: rgba(119, 124, 135, 0.2) !important;
+				border: none;
+			}
+
+			.lens-nav a {
+				color: #FFF !important;
+				font-weight: normal;
+				font-size: 1em;
+				text-transform: uppercase;
+			}
+
+			.lens-nav .active > a{
+				background: transparent !important;
+			}
 		</style>
 	</head>
         <body>
-<!-- 
-			<nav class="navbar navbar-default" role="navigation">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="#">LENS</a>
-					</div>
-					<div class="collapse navbar-collapse">
-						<ul class="nav navbar-nav">
-							<li class="active"><a href="#slide-1">Slide 1</a></li>
-							<li><a href="#slide-2">Slide 2</a></li>
-						</ul>
-					</div><
-				</div>
-			</nav> -->
-			<div class="main-content">
-			<section id="slide-0" class="container-fluid text-center">
-					<div class="logo">Lens</div>
-					<hr>
-					<div class="slogan">Building WebApps since 1991</div>
-					<p>
-					  <button type="button" class="lens-btn">Large button</button>
-					  <button type="button" class="lens-btn">Large button</button>
-					  <button type="button" class="lens-btn">Large button</button>
-					</p>
 
+			<nav class="navbar navbar-default navbar-fixed-top lens-nav" role="navigation">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-10">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<svg width="32" height="24">
+									    <path d="M0,6 32,6" stroke="#FFF" stroke-width="2"/>
+									    <path d="M0,14 32,14" stroke="#FFF" stroke-width="2"/>
+									    <path d="M0,22 32,22" stroke="#FFF" stroke-width="2"/>
+  									</svg>
+								</button>
+								<a class="navbar-brand" href="#">LENS</a>
+							</div>
+							<div class="collapse navbar-collapse navbar-right">
+								<ul class="nav navbar-nav">
+									<li><a href="#slide-1">¿lens?</a></li>
+									<li><a href="#slide-2">Productos</a></li>
+									<li><a href="#slide-3">Testimonios</a></li>
+									<li><a href="#slide-5">Contacto</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+
+			<div class="main-content">
+
+			<section id="slide-0" class="container-fluid text-center">
+
+					<div class="row"><div class="logo">Lens</div></div>
+					<hr>
+					<div class="row"><div class="slogan">Building WebApps since 1991</div></div>
+					<div class="row">
+					  <button type="button" class="lens-btn">Large button</button>
+					  <button type="button" class="lens-btn">Large button</button>
+					  <button type="button" class="lens-btn">Large button</button>
+					</div>
 			</section>
 
-			<section id="slide-1" class="bcg container-fluid text-center">
-		
-				    <div style="position: relative;">
-				    	<p class="title">Mobile Development</p>
-				    	<img src="img/android.png" />
-				    	<img src="img/ios.png" />
-				    </div>
-	
+			<section id="slide-1">
+			</section>
 
+			<section id="slide-2">
+			    <div class="container text-center" >
+			    	<div class="row">
+					   	<div class="col-md-6">
+					   		<p class="title">Mobile Development</p>
+					   		<div class="row">
+							   	<div class="col-md-6"><img class="img-rounded" src="img/android.png" /></div>
+						   		<div class="col-md-6"><img class="img-rounded" src="img/ios.png" /></div>
+					   		</div>
+					   	</div>
+					   	<div class="col-md-6">
+					   		<p class="title">Web Development</p>
+   					   		<div class="row">
+							   	<div class="col-md-6"><img class="img-rounded" src="img/android.png" /></div>
+						   		<div class="col-md-6"><img class="img-rounded" src="img/ios.png" /></div>
+					   		</div>
+					   	</div>
+
+					</div>		
+			    </div>
 			</section>
 			
-			<section id="slide-2">
-			</section>
 
-			<section></section>
-			<section id="slide-5">
+			<section id="slide-3"></section>
+			<section id="slide-5" class="no-resize">
 					<p
 					>
 						Alg&uacute;n footer
